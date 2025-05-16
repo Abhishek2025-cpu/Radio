39c75c6e5f6d92cd3a28933286de46eaec65fd07
Abhishek <abhisheks@pearlorganisation.com>
2025-05-16 15:48:04 +0530

    new push

--- routes/app/banner.routes.js
+++ routes/app/banner.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createBanner, getBanners } = require('../../controllers/bannerController');
+const { createBanner, getBanners } = require('../../controllers/app/banner.controller');
 const { uploadBannerMedia } = require('../../middleware/upload');
 
 // Expecting multiple images under field name "images"
