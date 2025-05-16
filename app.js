92a4132505d384c9ab5b0d7cffdd6b809ff15c30
Abhishek <abhisheks@pearlorganisation.com>
2025-05-16 15:29:43 +0530

    new

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 const siteBannerRoutes = require('./routes/site/banner.routes');
 const appBannerRoutes = require('./routes/app/banner.routes');
