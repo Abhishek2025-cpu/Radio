98e6d782d84010f94e6c62f311ed7e283564d6ca
Abhishek <abhisheks@pearlorganisation.com>
2025-05-16 15:26:46 +0530

    u

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const connectMongo = require('./config/db.mongo');
 
 
 const PORT = process.env.PORT || 2026;
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 (async () => {
   await connectMongo();
