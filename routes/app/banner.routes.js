27a84876413f78346ed13309061441270085ad29
Abhishek <abhisheks@pearlorganisation.com>
2025-05-23 11:30:07 +0530

    neww

--- routes/app/banner.routes.js
+++ routes/app/banner.routes.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const { createBanner, getBanners,updateBanner,updateBannerField,deleteBanner } = require('../../controllers/app/banner.controller');
-const upload = require('../../middlewares/upload');
+const multer = require('multer');
+const storage = multer.diskStorage({
+  filename: (req, file, cb) => cb(null, Date.now() + '-' + file.originalname)
+});
+const upload = multer({ storage });
 
-// Expecting multiple images under field name "images"
 router.post(
-  '/create-banner',
+  '/add-banner',
   upload.fields([
     { name: 'images', maxCount: 5 },
     { name: 'video', maxCount: 1 }
   ]),
-  createBanner
+  bannerController.createBanner
 );
+
 router.get('/get-banners', getBanners);
 router.put('/update-banner/:id', updateBanner);
 router.patch('/update-field/:id', updateBannerField);
