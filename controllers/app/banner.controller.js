0273223066f394997b553175a91b6fef754ce19c
Abhishek <abhisheks@pearlorganisation.com>
2025-05-23 11:54:30 +0530

    cloudninary variable

--- controllers/app/banner.controller.js
+++ controllers/app/banner.controller.js
@@ -1,5 +1,7 @@
 const AppBanner = require('../../models/mongo/AppBanner.model');// Placeholder content for AppBanner.model.js
-const saveBase64File = require('../../utils/cloudinary');
+const cloudinary = require('../../utils/cloudinary');
+console.log('req.files:', req.files);
+console.log('req.body:', req.body);
 
 exports.createBanner = async (req, res) => {
   try {
