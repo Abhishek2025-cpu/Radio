b345f76394d4a0b926bf196a6a08cad3cb9046d6
Abhishek <abhisheks@pearlorganisation.com>
2025-05-26 13:00:43 +0530

    test6

--- routes/app/news.routes.js
+++ routes/app/news.routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const multer = require('multer');
 const { storage } = require("../../utils/cloudinary");
 const upload = multer({ storage });
-// Upload fields: multiple images and one audio file
+
 router.post('/news', upload.fields([
   { name: 'images', maxCount: 5 },
   { name: 'audio', maxCount: 1 }
