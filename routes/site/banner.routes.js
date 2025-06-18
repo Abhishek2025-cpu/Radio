67c10087064bc3f882650fb3e5693c98e9b4d432
Abhishek <abhisheks@pearlorganisation.com>
2025-06-18 12:48:26 +0530

    text

--- routes/site/banner.routes.js
+++ routes/site/banner.routes.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer');
+
 const bannerController = require('../../controllers/site/banner.controller');
 
-// Multer setup for handling form-data
-const storage = multer.diskStorage({
-  filename: (req, file, cb) => cb(null, Date.now() + '-' + file.originalname)
-});
+const multer = require('multer');
+const { storage } = require('../../utils/cloudinary'); 
 const upload = multer({ storage });
 
-// POST: Create banner (images: array, video: single)
 router.post(
   '/add-banner',
   upload.fields([
