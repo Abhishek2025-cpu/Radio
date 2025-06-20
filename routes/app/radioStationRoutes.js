c7aa6512fec9930b53eb1ede9b785b0dd57f4980
Abhishek <abhisheks@pearlorganisation.com>
2025-06-20 17:07:24 +0530

    update and delete api for stations

--- routes/app/radioStationRoutes.js
+++ routes/app/radioStationRoutes.js
@@ -4,8 +4,8 @@ const controller = require("../../controllers/app/radioStationController");
 
 router.post("/add-stations", controller.addRadioStation);
 router.get("/get-stations", controller.getAllStations);
-router.put("update/:id", controller.updateStation);
-router.delete("delete/:id", controller.deleteStation);
+router.put("/update/:id", controller.updateStation);
+router.delete("/delete/:id", controller.deleteStation);
 router.patch("/:id/toggle", controller.toggleActive);
 
 module.exports = router;
