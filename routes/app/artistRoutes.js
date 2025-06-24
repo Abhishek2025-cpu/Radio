2b58b237a7974f5868996c6997b48f81eaa43863
Abhishek <abhisheks@pearlorganisation.com>
2025-06-24 10:40:41 +0530

    artists apis

--- routes/app/artistRoutes.js
+++ routes/app/artistRoutes.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const artistController = require('../../controllers/app/artistController');
 
 // CRUD
-router.post('/add-artist', artistController.createArtist);
+router.post('/add-artist', upload.single('profileImage'), artistController.createArtist);
+router.put('/update-artist/:id', upload.single('profileImage'), artistController.updateArtist);
 router.get('/get-all-artists', artistController.getAllArtists);
 router.get('/get-artist/:id', artistController.getArtistById);
-router.put('update-artist/:id', artistController.updateArtist);
-router.patch('active-artist/:id', artistController.partialUpdateArtist);
-router.delete('delete-artist/:id', artistController.deleteArtist);
+
+router.patch('/active-artist/:id', artistController.partialUpdateArtist);
+router.delete('/delete-artist/:id', artistController.deleteArtist);
 
 // Voting
 router.post('/vote/:id', artistController.voteArtist);
