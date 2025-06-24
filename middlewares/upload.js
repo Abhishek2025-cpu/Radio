8c3dc9aa7fe554114f8b9c941a4d75d06a2ec95d
Abhishek <abhisheks@pearlorganisation.com>
2025-06-24 12:15:02 +0530

    test4

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,21 +1,7 @@
+// middlewares/upload.js
 const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
 
-// Ensure uploads folder exists
-const uploadPath = path.join(__dirname, '../uploads');
-if (!fs.existsSync(uploadPath)) fs.mkdirSync(uploadPath);
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, uploadPath);
-  },
-  filename: function (req, file, cb) {
-    const ext = path.extname(file.originalname);
-    const name = `${file.fieldname}-${Date.now()}${ext}`;
-    cb(null, name);
-  }
-});
+const storage = multer.memoryStorage(); // ✅ in-memory storage
 
 const upload = multer({ storage });
 
