9a04040e3775510375d67c6123b18262c6aee459
Abhishek <abhisheks@pearlorganisation.com>
2025-06-27 13:51:56 +0530

    controllar name updated

--- routes/app/radio.js
+++ routes/app/radio.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const radioController = require('../../controllers/app/getAllStationMetadata'); 
 
 // Single route for all 6 stations
-router.get('/stations/metadata', radioController.getAllRadioStationMetadata);
+router.get('/stations/metadata', radioController.getAllStationMetadata);
 
 module.exports = router;
