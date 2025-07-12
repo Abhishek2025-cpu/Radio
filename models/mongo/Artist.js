5a0f4de37daf0f197cb7e6691fa780972aeac10e
Abhishek <abhisheks@pearlorganisation.com>
2025-07-12 15:38:33 +0530

    schema updated

--- models/mongo/Artist.js
+++ models/mongo/Artist.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Website = require('./Website');
 
 const artistSchema = new mongoose.Schema({
   name: String,
@@ -16,14 +17,30 @@ const artistSchema = new mongoose.Schema({
       }
     ],
     default: [],
-
   },
-
-    mediaUrl: { type: String }, 
+  mediaUrl: { type: String },
   isActive: {
     type: Boolean,
     default: true,
   },
 }, { timestamps: true });
+
+artistSchema.post('save', async function (doc, next) {
+  try {
+    // Check if a Website already exists for this artist name to avoid duplicates
+    const existingWebsite = await Website.findOne({ name: doc.name });
+    if (!existingWebsite) {
+      await Website.create({
+        name: doc.name,
+        url: doc.profileImage || '', // or any other value you prefer
+        votes: doc.votes,
+      });
+    }
+    next();
+  } catch (error) {
+    console.error('Error copying artist to website:', error);
+    next(error);
+  }
+});
 
 module.exports = mongoose.model('Artist', artistSchema);
