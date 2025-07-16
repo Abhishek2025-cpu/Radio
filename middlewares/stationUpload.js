4c9f83a673a8c3af7080526dd483921298d638c6
Abhishek <abhisheks@pearlorganisation.com>
2025-07-16 14:11:23 +0530

    station

--- middlewares/stationUpload.js
+++ middlewares/stationUpload.js
@@ -1,22 +1,15 @@
 const multer = require('multer');
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require('../utils/cloudinary'); // Direct cloudinary instance
 
-// 1. We import the entire object you exported { cloudinary, uploadToCloudinary }
-//    Let's rename it to be clear about what it is.
-const cloudinaryConfig = require('../utils/cloudinary'); 
-
-// Configure Multer to use Cloudinary for storage
 const storage = new CloudinaryStorage({
-  // 2. THE FIX: Instead of passing the whole object, we pass the 'cloudinary' property from it.
-  cloudinary: cloudinaryConfig.cloudinary, 
-  
+  cloudinary,
   params: {
     folder: 'station-thumbnails',
     allowed_formats: ['jpeg', 'jpg', 'png'],
   }
 });
 
-// Create and export the Multer instance. This part doesn't change.
-const stationThumbnailUploader = multer({ storage: storage });
+const stationThumbnailUploader = multer({ storage });
 
 module.exports = stationThumbnailUploader;
