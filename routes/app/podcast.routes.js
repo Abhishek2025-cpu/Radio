a43f05398d226551f433894fb0afc2018aa14abc
Abhishek <abhisheks@pearlorganisation.com>
2025-07-16 18:16:41 +0530

    id

--- routes/app/podcast.routes.js
+++ routes/app/podcast.routes.js
@@ -88,7 +88,7 @@ router.patch('/toggle-status/:id', togglePodcastStatus);
 //////////////////////////////////shows-routes///////////////////////////////////////////////////////////
 router.post("/:genreName/add", uploadStation.single("image"), addShowToGenre);
 router.get("/",getAllGenreShows);
-router.put("/show/toggle/:identifier", toggleGenreShow);
+router.put("/:genreName/toggle/:identifier",toggleGenreShow);
 router.delete("/delete/:identifier", deleteGenreShow);
 router.put("/:genreName/update/:showId", uploadStation.single("image"),updateGenreShow);
 
