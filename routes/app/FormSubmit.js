5d52575db401c7ebd363ff70ecf578fc8cf7ad30
Abhishek <abhisheks@pearlorganisation.com>
2025-07-18 17:03:34 +0530

    upload file

--- routes/app/FormSubmit.js
+++ routes/app/FormSubmit.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer');
+
 const formController = require('../../controllers/app/form.controller');
 
 // const storage = multer.diskStorage({
